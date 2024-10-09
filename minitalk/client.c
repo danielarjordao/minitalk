584b4979af685b39d8ef4e3ee69da5582ced6fdd
danielarjordao <danielarjordao@hotmail.com>
2024-10-09 12:18:36 +0100

    test sigaction

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,20 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dramos-j <dramos-j@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dramos-j <dramos-j@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:43:48 by dramos-j          #+#    #+#             */
-/*   Updated: 2024/10/08 17:20:11 by dramos-j         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:17:29 by dramos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_size(int pid, char *message)
+{
+	int	len;
+	int	i;
+
+	len = ft_strlen(message);
+	i = 31;
+	while (i >= 0)
+	{
+		if (len & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(200);
+		i--;
+	}
+}
+
+void	ack_handler(int signum)
+{
+	(void)signum;
+}
+
 void	send_message(int pid, char *message)
 {
 	int	i;
 	int	j;
+	struct sigaction	sa;
 
+	sa.sa_flags = 0;
+	sa.sa_handler = &ack_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	i = 0;
 	while (message[i])
 	{
@@ -27,22 +55,24 @@ void	send_message(int pid, char *message)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(500);
+			usleep(200);
 			j--;
 		}
 		i++;
+		pause();
 	}
-	message[i] = '\n';
 	j = 7;
 	while (j >= 0)
 	{
-		if (message[i] & (1 << j))
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(500);
+		kill(pid, SIGUSR2);
+		usleep(200);
 		j--;
 	}
+}
+
+void	handler_client(int signum)
+{
+	(void)signum;
 }
 
 int	main(int argc, char **argv)
@@ -51,18 +81,10 @@ int	main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		pid = ft_atoi(argv[1]);
-		if (pid <= 0)
-		{
-			ft_printf("Invalid PID\n");
-			return (1);
-		}
+		pid = ft_atoi(argv[1]);send_size(pid, argv[2]);
 		send_message(pid, argv[2]);
 	}
 	else
-	{
 		ft_printf("Usage: ./client [PID] [message]\n");
-		return (1);
-	}
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,20 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dramos-j <dramos-j@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dramos-j <dramos-j@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 13:11:30 by dramos-j          #+#    #+#             */
-/*   Updated: 2024/10/08 17:16:37 by dramos-j         ###   ########.fr       */
+/*   Updated: 2024/10/09 12:13:00 by dramos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handler(int signum)
+static int	g_len = 0;
+
+void	handler_len(int signum)
+{
+	static int	temp_len = 0;
+	static int	bit = 0;
+
+	if (signum == SIGUSR1)
+		temp_len = (temp_len << 1) | 1;
+	else
+		temp_len = (temp_len << 1);
+	bit++;
+	if (bit == 32)
+	{
+		g_len = temp_len;
+		ft_printf("Message length: %d\n", g_len);
+		if (g_len == 0)
+			g_len = -1;
+		temp_len = 0;
+		bit = 0;
+	}
+}
+
+void	handler(int signum, siginfo_t *info, void *context)
 {
 	static char	c = 0;
 	static int	bit = 0;
+	static int	j = 0;
+	static char	*str = NULL;
 
+	(void)context;
+	if (!str && g_len > 0)
+		str = ft_calloc(g_len + 1, sizeof(char));
 	if (signum == SIGUSR1)
 		c = c << 1 | 1;
 	else
@@ -24,7 +52,22 @@ void	handler(int signum)
 	bit++;
 	if (bit == 8)
 	{
-		ft_printf("%c", c);
+		if (j < g_len)
+			str[j++] = c;
+		if (c == '\0')
+		{
+			if (str)
+			{
+				str[j] = '\0';
+				ft_printf("%s\n", str);
+				free(str);
+			}
+			str = NULL;
+			g_len = 0;
+			j = 0;
+		}
+		if (kill(info->si_pid, SIGUSR1) == -1)
+			ft_printf("Error sending signal\n");
 		c = 0;
 		bit = 0;
 	}
@@ -33,7 +76,18 @@ void	handler(int signum)
 int	main(void)
 {
 	int	pid;
+	struct sigaction	sa_len;
+	struct sigaction	sa;
 
+
+	sa_len.sa_handler = handler_len;
+	sa_len.sa_flags = 0;
+	sigaction(SIGUSR1, &sa_len, NULL);
+	sigaction(SIGUSR2, &sa_len, NULL);
+	sa.sa_sigaction = handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	pid = getpid();
 	if (pid < 0)
 	{
@@ -42,8 +96,7 @@ int	main(void)
 	}
 	else
 		ft_printf("PID: %d\n", getpid());
-	signal(SIGUSR1, handler);
-	signal(SIGUSR2, handler);
 	while (1)
 		pause();
+	return (0);
 }
